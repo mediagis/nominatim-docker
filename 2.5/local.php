ac88a86986aafa74f3ec7fd78ff7af6fe2b9b060
Andrew <pipetc@gmail.com>
2017-01-18 19:16:37 +0300

    Re #5 set CONST_Replication_Url for Monaco by default.

--- 2.5/local.php
+++ 2.5/local.php
@@ -4,7 +4,7 @@
  @define('CONST_Postgis_Version', '2.1');
  // Website settings
  @define('CONST_Website_BaseURL', '/');
- // @define('CONST_Replication_Url', 'http://download.geofabrik.de/europe-updates');
+ @define('CONST_Replication_Url', 'http://download.geofabrik.de/monaco-updates');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
