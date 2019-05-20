05532d6493deb3cad66fc41a4715162c3f6858e9
Mateusz Gaweł <mattegawel@gmail.com>
2019-05-20 09:51:09 +0200

    Use a required connection string style in new Postgres

--- 3.3/local.php
+++ 3.3/local.php
@@ -10,6 +10,5 @@
  @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
  @define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
 
- //@define('CONST_Database_DSN', 'pgsql://nominatim:password1234@192.168.1.128:6432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
-
+ //@define('CONST_Database_DSN', 'pgsql:host=192.168.1.128;port=6432;user=nominatim;password=password1234;dbname=nominatim'); <driver>:host=<host>;port=<port>;user=<username>;password=<password>;dbname=<database>
 ?>
