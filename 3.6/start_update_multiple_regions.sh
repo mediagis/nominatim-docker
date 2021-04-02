bf629848ceabd9b4d20b20ecd3c99e594ec5b6ff
Maxime Charruel <m.charruel@gmail.com>
2021-04-02 11:26:33 +0200

    Add manage update multiple regions

--- 3.6/start_update_multiple_regions.sh
+++ 3.6/start_update_multiple_regions.sh
@@ -7,5 +7,5 @@ sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR start && \
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
 chown -R nominatim:nominatim ./nominatim && \
-sudo -u nominatim sh ./src/build/utils/add_multiple_regions.sh && \ 
+sudo -u nominatim sh ./src/build/utils/update_multiple_regions.sh && \
 sudo -u postgres /usr/lib/postgresql/12/bin/pg_ctl -D /data/$PGDIR stop
