470e7544a29eafa3bb1a12043baea6969b8bd770
Philipp Gassmann <phiphi@phiphi.ch>
2022-09-06 21:03:32 +0200

    warm cache on start after init

--- 4.1/start.sh
+++ 4.1/start.sh
@@ -47,4 +47,8 @@ fi
 # fork a process and wait for it
 tail -Fv /var/log/postgresql/postgresql-14-main.log /var/log/apache2/access.log /var/log/apache2/error.log /var/log/replication.log &
 tailpid=${!}
+
+echo "Warm database caches for search and reverse queries"
+sudo -E -u nominatim nominatim admin --warm > /dev/null
+echo "Warming finished"
 wait
