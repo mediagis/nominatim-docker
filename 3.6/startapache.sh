7299ad64d24d216fa3349b3ebf65a297babf32e7
Philip Kozeny <philip.kozeny@symvaro.com>
2025-07-25 13:16:37 +0200

    refactor git strategy (see: https://github.com/mediagis/nominatim-docker/discussions/627)

--- 3.6/startapache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp /data/local.php /app/src/build/settings/local.php
-/app/src/build/utils/setup.php --setup-website
-
-/usr/sbin/apache2ctl -D FOREGROUND
-tail -f /var/log/apache2/error.log
